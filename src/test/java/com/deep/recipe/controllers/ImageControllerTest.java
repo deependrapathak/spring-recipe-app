8e3e488c222c9ca06fbe2692d397ee16c4cdc09f
Deependra Pathak <deependrarajpathak@gmail.com>
2020-07-11 14:30:18 -0400

    [master]Added image upload functionlity

--- src/test/java/com/deep/recipe/controllers/ImageControllerTest.java
+++ src/test/java/com/deep/recipe/controllers/ImageControllerTest.java
@@ -1,13 +1,20 @@
 package com.deep.recipe.controllers;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import static org.mockito.Mockito.*;
 import org.mockito.MockitoAnnotations;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -19,58 +26,85 @@ import com.deep.recipe.services.ImageService;
 import com.deep.recipe.services.RecipeService;
 
 class ImageControllerTest {
-	
+
 	@Mock
 	ImageService imageService;
 
 	@Mock
 	RecipeService recipeService;
-	
+
 	ImageController controller;
-	
+
 	MockMvc mockMvc;
-	
+
 	@BeforeEach
 	void setUp() throws Exception {
 		MockitoAnnotations.initMocks(this);
-		
+
 		controller = new ImageController(imageService, recipeService);
-		
+
 		mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
 	}
 
 	@Test
 	void getImageForm() throws Exception {
-		
-		//given
+
+		// given
 		RecipeCommand command = new RecipeCommand();
-		
+
 		command.setId(1L);
-		
+
 		Mockito.when(recipeService.findCommandById(anyLong())).thenReturn(command);
-		
-		//when
-		
-		mockMvc.perform(MockMvcRequestBuilders.get("/recipe/1/image"))
-				.andExpect(MockMvcResultMatchers.status().isOk())
+
+		// when
+
+		mockMvc.perform(MockMvcRequestBuilders.get("/recipe/1/image")).andExpect(MockMvcResultMatchers.status().isOk())
 				.andExpect(MockMvcResultMatchers.model().attributeExists("recipe"));
-		
+
 		verify(recipeService, times(1)).findCommandById(anyLong());
-		
-		
+
 	}
-	
+
 	@Test
-    public void handleImagePost() throws Exception {
-		
-		MockMultipartFile multipartFile = new MockMultipartFile("imageFile", "testing.txt", "text/plain", 
+	public void handleImagePost() throws Exception {
+
+		MockMultipartFile multipartFile = new MockMultipartFile("imageFile", "testing.txt", "text/plain",
 				"Spring Framework Guru".getBytes());
-		
+
 		mockMvc.perform(MockMvcRequestBuilders.multipart("/recipe/1/image").file(multipartFile))
 				.andExpect(MockMvcResultMatchers.status().is3xxRedirection())
 				.andExpect(MockMvcResultMatchers.header().string("Location", "/recipe/1/show"));
-		
-		 verify(imageService, times(1)).saveImageFile(anyLong(), any());
+
+		verify(imageService, times(1)).saveImageFile(anyLong(), any());
+	}
+
+	@Test
+	public void renderImageFromDB() throws Exception {
+
+		// given
+		RecipeCommand command = new RecipeCommand();
+		command.setId(1L);
+
+		String s = "fake image text";
+		Byte[] bytesBoxed = new Byte[s.getBytes().length];
+
+		int i = 0;
+
+		for (byte primByte : s.getBytes()) {
+			bytesBoxed[i++] = primByte;
+		}
+
+		command.setImage(bytesBoxed);
+
+		when(recipeService.findCommandById(anyLong())).thenReturn(command);
+
+		// when
+		MockHttpServletResponse response = mockMvc.perform(get("/recipe/1/recipeimage")).andExpect(status().isOk())
+				.andReturn().getResponse();
+
+		byte[] reponseBytes = response.getContentAsByteArray();
+
+		assertEquals(s.getBytes().length, reponseBytes.length);
 	}
 
 }
